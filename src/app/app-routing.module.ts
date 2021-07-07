54d6276bb7f6bd0f0ebfe2d25822fbb1a0e7edd9
Zartab Nakhwa <nzartab@gmail.com>
2021-07-07 10:06:26 +0530

    Added Routes and used router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerAddComponent } from './customers/customer-add/customer-add.component';
+import { CustomerDisplayComponent } from './customers/customer-display/customer-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add',component:CustomerAddComponent},
+  {path:'display',component:CustomerDisplayComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
