7ecd54a805fbe795bdc21d90da252e5b41158007
Zartab Nakhwa <nzartab@gmail.com>
2021-07-07 09:27:36 +0530

    Customer Model added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerAddComponent } from './customers/customer-add/customer-add.component';
+import { CustomerDisplayComponent } from './customers/customer-display/customer-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerAddComponent,
+    CustomerDisplayComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/customers/customer.model.ts
@@ -0,0 +1,14 @@
+export class Customer{
+   constructor(
+        public name:string,
+        public age:number,
+        public company:string,
+        public email:string,
+        public picture:string,
+        public balance:number
+
+   ){
+
+   }
+
+}
